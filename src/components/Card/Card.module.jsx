d7cb9bb60a38611ce5b185cf23d5ba18c7451dae
Nakami <natalia.rivas@correo.unimet.edu.ve>
2023-06-10 09:20:34 -0400

    mm

---
+++ src/components/Card/Card.module.jsx
@@ -0,0 +1,12 @@
+import styles from "./Card.module.css"
+
+
+export function Card() {
+    return (
+        <div className={styles.container}>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
