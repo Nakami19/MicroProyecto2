fe1dfbf67bef7176d09f9b0ea4f7143ada97e1aa
BomeLoL <kli@correo.unimet.edu.ve>
2023-06-10 15:01:53 -0400

    slider

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import { useMovies } from "../../hooks/useMovies";
 import styles from "./HomePage.module.css"
 import { Card } from "../../components/Card/Card";
+import { Carousel } from "../../components/Carrousel/Carousel";
+import { slides } from "../../components/Carrousel/SliderData.json";
+
 
 export function HomePage() {
     const {
@@ -16,6 +19,9 @@ export function HomePage() {
 
     return (
         <div className={styles.container}>
+            <div className={styles.hero}>
+            <Carousel data={slides} />
+            </div>
         <h1 className={styles.title}>Peliculas</h1>
         <div className={styles.movies}>
           {
